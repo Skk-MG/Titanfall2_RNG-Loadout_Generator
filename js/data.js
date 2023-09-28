2bee2555694026a6ac6e444d9e0cab627f12249f
Skk-martinGarcia <martinezequielgarcia495@gmail.com>
2023-09-28 11:18:33 -0300

    test botonB

--- js/data.js
+++ js/data.js
@@ -1,76 +1,76 @@
 const pilotSkill = [
-    {nombreSkill: "A-Wall", imagenSkill: "../assets/tactical/Tacbanner_awall.webp"},
-    {nombreSkill: "Cloak", imagenSkill: "../assets/tactical/Tacbanner_cloak.webp"},
-    {nombreSkill: "Grapple", imagenSkill: "../assets/tactical/Tacbanner_grapple.webp"},
-    {nombreSkill: "Holo Pilot", imagenSkill: "../assets/tactical/Tacbanner_holopilot.webp"},
-    {nombreSkill: "Phase Shift", imagenSkill: "../assets/tactical/Tacbanner_phaseshift.webp"},
-    {nombreSkill: "Pulse Blade", imagenSkill: "../assets/tactical/Tacbanner_pulseblade.webp"},       
-    {nombreSkill: "Stim", imagenSkill: "../assets/tactical/Tacbanner_stim.webp"}
+    {nombreSkill: "A-Wall", imagenSkill: "./assets/tactical/Tacbanner_awall.webp"},
+    {nombreSkill: "Cloak", imagenSkill: "./assets/tactical/Tacbanner_cloak.webp"},
+    {nombreSkill: "Grapple", imagenSkill: "./assets/tactical/Tacbanner_grapple.webp"},
+    {nombreSkill: "Holo Pilot", imagenSkill: "./assets/tactical/Tacbanner_holopilot.webp"},
+    {nombreSkill: "Phase Shift", imagenSkill: "./assets/tactical/Tacbanner_phaseshift.webp"},
+    {nombreSkill: "Pulse Blade", imagenSkill: "./assets/tactical/Tacbanner_pulseblade.webp"},       
+    {nombreSkill: "Stim", imagenSkill: "./assets/tactical/Tacbanner_stim.webp"}
 ];
 
 const pilotBoost = [
-    {nombreBoost: "Amped Weapons", imagenBoost: "../assets/boosts/Boost_ampedweapons.webp"},
-    {nombreBoost: "Ticks", imagenBoost: "../assets/boosts/Boost_ticks.webp"},
-    {nombreBoost: "Pilot Sentry", imagenBoost: "../assets/boosts/Boost_pilotsentry.webp"},
-    {nombreBoost: "Map Hack", imagenBoost: "../assets/boosts/Boost_maphack.webp"},
-    {nombreBoost: "Battery Back-Up", imagenBoost: "../assets/boosts/Boost_batterybackup.webp"},
-    {nombreBoost: "Radar Jammer", imagenBoost: "../assets/boosts/Boost_radarjammer.webp"},
-    {nombreBoost: "Titan Sentry", imagenBoost: "../assets/boosts/Boost_titansentry.webp"},
-    {nombreBoost: "Smart Pistol MK6", imagenBoost: "../assets/boosts/Boost_smartpistol.webp"},
-    {nombreBoost: "Phase Rewind", imagenBoost: "../assets/boosts/Boost_phaserewind.webp"},
-    {nombreBoost: "Hard Cover", imagenBoost: "../assets/boosts/Boost_hardcover.webp"},
-    {nombreBoost: "Holo Pilot Nova", imagenBoost: "../assets/boosts/Boost_holopilotnova.webp"},
-    {nombreBoost: "Dice Roll", imagenBoost: "../assets/boosts/Boost_diceroll.webp"}
+    {nombreBoost: "Amped Weapons", imagenBoost: "./assets/boosts/Boost_ampedweapons.webp"},
+    {nombreBoost: "Ticks", imagenBoost: "./assets/boosts/Boost_ticks.webp"},
+    {nombreBoost: "Pilot Sentry", imagenBoost: "./assets/boosts/Boost_pilotsentry.webp"},
+    {nombreBoost: "Map Hack", imagenBoost: "./assets/boosts/Boost_maphack.webp"},
+    {nombreBoost: "Battery Back-Up", imagenBoost: "./assets/boosts/Boost_batterybackup.webp"},
+    {nombreBoost: "Radar Jammer", imagenBoost: "./assets/boosts/Boost_radarjammer.webp"},
+    {nombreBoost: "Titan Sentry", imagenBoost: "./assets/boosts/Boost_titansentry.webp"},
+    {nombreBoost: "Smart Pistol MK6", imagenBoost: "./assets/boosts/Boost_smartpistol.webp"},
+    {nombreBoost: "Phase Rewind", imagenBoost: "./assets/boosts/Boost_phaserewind.webp"},
+    {nombreBoost: "Hard Cover", imagenBoost: "./assets/boosts/Boost_hardcover.webp"},
+    {nombreBoost: "Holo Pilot Nova", imagenBoost: "./assets/boosts/Boost_holopilotnova.webp"},
+    {nombreBoost: "Dice Roll", imagenBoost: "./assets/boosts/Boost_diceroll.webp"}
 ];
 
 const pilotNades = [
-    {nombreNades: "Frag Grenade", imagenNades: "../assets/throwables/Fraggrenade.webp"},
-    {nombreNades: "Arc Grenade", imagenNades: "../assets/throwables/ArcT2.webp"},
-    {nombreNades: "Electric Smoke Grenade", imagenNades: "../assets/throwables/Electric_Smoke_Grenade.webp"},
-    {nombreNades: "Gravity Star", imagenNades: "../assets/throwables/Gravity_Star.webp"},
-    {nombreNades: "Firestar", imagenNades: "../assets/throwables/Firestar.webp"},
-    {nombreNades: "Satchel", imagenNades: "../assets/throwables/SatchelT2.webp"}
+    {nombreNades: "Frag Grenade", imagenNades: "./assets/throwables/Fraggrenade.webp"},
+    {nombreNades: "Arc Grenade", imagenNades: "./assets/throwables/ArcT2.webp"},
+    {nombreNades: "Electric Smoke Grenade", imagenNades: "./assets/throwables/Electric_Smoke_Grenade.webp"},
+    {nombreNades: "Gravity Star", imagenNades: "./assets/throwables/Gravity_Star.webp"},
+    {nombreNades: "Firestar", imagenNades: "./assets/throwables/Firestar.webp"},
+    {nombreNades: "Satchel", imagenNades: "./assets/throwables/SatchelT2.webp"}
 ];
 
 const mainWPN = [
-    {nombreWPN: "R-201 Carbine", clase: "AR", imagenWPN: "../assets/wpn/Icon_r201.webp"},
-    {nombreWPN: "R-101 Carbine", clase: "AR", imagenWPN: "../assets/wpn/Icon_r101.webp"},
-    {nombreWPN: "Hemlok BF-R", clase: "AR", imagenWPN: "../assets/wpn/Icon_hemlok.webp"},
+    {nombreWPN: "R-201 Carbine", clase: "AR", imagenWPN: "./assets/wpn/Icon_r201.webp"},
+    {nombreWPN: "R-101 Carbine", clase: "AR", imagenWPN: "./assets/wpn/Icon_r101.webp"},
+    {nombreWPN: "Hemlok BF-R", clase: "AR", imagenWPN: "./assets/wpn/Icon_hemlok.webp"},
 
-    {nombreWPN: "CAR", clase: "SMG", imagenWPN: "../assets/wpn/Icon_car.webp"},
-    {nombreWPN: "Alternator", clase: "SMG", imagenWPN: "../assets/wpn/Icon_alternator.webp"},
-    {nombreWPN: "Volt", clase: "SMG", imagenWPN: "../assets/wpn/Icon_volt.webp"},
+    {nombreWPN: "CAR", clase: "SMG", imagenWPN: "./assets/wpn/Icon_car.webp"},
+    {nombreWPN: "Alternator", clase: "SMG", imagenWPN: "./assets/wpn/Icon_alternator.webp"},
+    {nombreWPN: "Volt", clase: "SMG", imagenWPN: "./assets/wpn/Icon_volt.webp"},
     
-    {nombreWPN: "Spitfire", clase: "LMG", imagenWPN: "../assets/wpn/Icon_spitfire.webp"},
-    {nombreWPN: "L-STAR", clase: "LMG", imagenWPN: "../assets/wpn/Icon_lstar.webp"},
-    {nombreWPN: "X-55 Devotion", clase: "LMG", imagenWPN: "../assets/wpn/Icon_devotion.webp"},
+    {nombreWPN: "Spitfire", clase: "LMG", imagenWPN: "./assets/wpn/Icon_spitfire.webp"},
+    {nombreWPN: "L-STAR", clase: "LMG", imagenWPN: "./assets/wpn/Icon_lstar.webp"},
+    {nombreWPN: "X-55 Devotion", clase: "LMG", imagenWPN: "./assets/wpn/Icon_devotion.webp"},
 
-    {nombreWPN: "Kraber-AP Sniper", clase: "RF", imagenWPN: "../assets/wpn/Icon_kraber.webp"},
-    {nombreWPN: "D-2 Double Take", clase: "RF", imagenWPN: "../assets/wpn/Icon_doubletake.webp"},
-    {nombreWPN: "Longbow DMR", clase: "RF", imagenWPN: "../assets/wpn/Icon_dmr.webp"},
+    {nombreWPN: "Kraber-AP Sniper", clase: "RF", imagenWPN: "./assets/wpn/Icon_kraber.webp"},
+    {nombreWPN: "D-2 Double Take", clase: "RF", imagenWPN: "./assets/wpn/Icon_doubletake.webp"},
+    {nombreWPN: "Longbow DMR", clase: "RF", imagenWPN: "./assets/wpn/Icon_dmr.webp"},
 
-    {nombreWPN: "EVA-8 Shotgun", clase: "SG", imagenWPN: "../assets/wpn/Icon_eva8.webp"},
-    {nombreWPN: "Mastiff", clase: "SG", imagenWPN: "../assets/wpn/Icon_mastiff.webp"},
+    {nombreWPN: "EVA-8 Shotgun", clase: "SG", imagenWPN: "./assets/wpn/Icon_eva8.webp"},
+    {nombreWPN: "Mastiff", clase: "SG", imagenWPN: "./assets/wpn/Icon_mastiff.webp"},
 
-    {nombreWPN: "Sidewinder SMR", clase: "GL", imagenWPN: "../assets/wpn/Icon_smr.webp"},
-    {nombreWPN: "EPG-1", clase: "GL", imagenWPN: "../assets/wpn/Icon_epg.webp"},
-    {nombreWPN: "R-6P Softball", clase: "GL", imagenWPN: "../assets/wpn/Icon_softball.webp"},
+    {nombreWPN: "Sidewinder SMR", clase: "GL", imagenWPN: "./assets/wpn/Icon_smr.webp"},
+    {nombreWPN: "EPG-1", clase: "GL", imagenWPN: "./assets/wpn/Icon_epg.webp"},
+    {nombreWPN: "R-6P Softball", clase: "GL", imagenWPN: "./assets/wpn/Icon_softball.webp"},
 
-    {nombreWPN: "Wingman Elite", clase: "HG", imagenWPN: "../assets/wpn/Icon_wingmanelite.webp"},
-    {nombreWPN: "SA-3 Mozambique", clase: "HG", imagenWPN: "../assets/wpn/Icon_mozambique.webp"}
+    {nombreWPN: "Wingman Elite", clase: "HG", imagenWPN: "./assets/wpn/Icon_wingmanelite.webp"},
+    {nombreWPN: "SA-3 Mozambique", clase: "HG", imagenWPN: "./assets/wpn/Icon_mozambique.webp"}
 ];
 
 const sideWPN = [
-    {nombreSIDE: "RE-45 Auto", imagenSIDE: "../assets/wpn/Icon_re45.webp"},
-    {nombreSIDE: "Hammond P2016", imagenSIDE: "../assets/wpn/Icon_p2016.webp"},
-    {nombreSIDE: "B3 Wingman", imagenSIDE: "../assets/wpn/Icon_b3wingman.webp"}
+    {nombreSIDE: "RE-45 Auto", imagenSIDE: "./assets/wpn/Icon_re45.webp"},
+    {nombreSIDE: "Hammond P2016", imagenSIDE: "./assets/wpn/Icon_p2016.webp"},
+    {nombreSIDE: "B3 Wingman", imagenSIDE: "./assets/wpn/Icon_b3wingman.webp"}
 ];
 
 const titanWPN = [
-    {nombreTWPN: "Charge Rifle", imagenTWPN: "../assets/wpn/Icon_chargerifle.webp"},
-    {nombreTWPN: "MGL Mag Launcher", imagenTWPN: "../assets/wpn/Icon_mgl.webp"},
-    {nombreTWPN: "LG-97 Thunderbolt", imagenTWPN: "../assets/wpn/Icon_thunderbolt.webp"},
-    {nombreTWPN: "Archer", imagenTWPN: "../assets/wpn/Icon_archer.webp"}
+    {nombreTWPN: "Charge Rifle", imagenTWPN: "./assets/wpn/Icon_chargerifle.webp"},
+    {nombreTWPN: "MGL Mag Launcher", imagenTWPN: "./assets/wpn/Icon_mgl.webp"},
+    {nombreTWPN: "LG-97 Thunderbolt", imagenTWPN: "./assets/wpn/Icon_thunderbolt.webp"},
+    {nombreTWPN: "Archer", imagenTWPN: "./assets/wpn/Icon_archer.webp"}
 ];
 
 const wpnMODS = [
